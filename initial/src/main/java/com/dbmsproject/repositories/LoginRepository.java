216f65d8e562d169711c52bf3506e6f7131ce12a
hongiranabm <hongiranabm@gmail.com>
2024-01-29 19:44:33 +0530

    changed spelling mistake

--- initial/src/main/java/com/dbmsproject/repositories/LoginRepository.java
+++ initial/src/main/java/com/dbmsproject/repositories/LoginRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.dbmsproject.dao.LoginDao;
 
-public interface LocationRepository extends CrudRepository <LoginDao, Integer> {
+public interface LoginRepository extends CrudRepository <LoginDao, Integer> {
     
 }
