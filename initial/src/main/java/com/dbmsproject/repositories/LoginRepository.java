36b55e5c7debe38e9e531234e9cdbd78000f32d0
Rehan Ahmed <rehan29.ahmed11@gmail.com>
2024-01-29 18:42:26 +0530

    login and parklotcapacity changes

--- initial/src/main/java/com/dbmsproject/repositories/LoginRepository.java
+++ initial/src/main/java/com/dbmsproject/repositories/LoginRepository.java
@@ -1,5 +1,9 @@
 package com.dbmsproject.repositories;
 
-public class LoginRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.dbmsproject.dao.LoginDao;
+
+public interface LocationRepository extends CrudRepository <LoginDao, Integer> {
     
 }
