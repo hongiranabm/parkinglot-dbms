72e697d6b571efbc0227767aa78ceb5ff1bd35bf
hongiranabm <hongiranabm@gmail.com>
2024-01-29 18:34:45 +0530

    more repositories done

--- initial/src/main/java/com/dbmsproject/business/InitializationService.java
+++ initial/src/main/java/com/dbmsproject/business/InitializationService.java
@@ -3,6 +3,10 @@ package com.dbmsproject.business;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dbmsproject.repositories.FaresRepository;
+import com.dbmsproject.repositories.LocationRepository;
+import com.dbmsproject.repositories.SlotTypeRepository;
+
 @Service
 public class InitializationService {
     @Autowired
@@ -13,5 +17,22 @@ public class InitializationService {
 
     @Autowired
     private FaresRepository faresRepository;
-    
+
+    public void initialize(){
+        initLocation();
+        initSlotTypes();
+        initFares();
+    }
+
+    private void initLocation(){
+        LocationRepository.save(new LocationDao());
+    }
+
+    private void initSlotTypes(){
+        LocationRepository.save(new SlotTypeDao());
+    }
+
+    private void initFares(){
+        LocationRepository.save(new FaresDao());
+    }
 }
