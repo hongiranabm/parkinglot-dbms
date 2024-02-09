79c6854d054d8cec1b49f16e91788e5cb23967c9
hongiranabm <hongiranabm@gmail.com>
2024-02-09 19:19:50 +0530

    Revert minor changes

--- initial/src/main/java/com/dbmsproject/controller/ParkingController.java
+++ initial/src/main/java/com/dbmsproject/controller/ParkingController.java
@@ -1,14 +1,26 @@
 package com.dbmsproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dbmsproject.business.InitializationService;
 
 @RestController
 public class ParkingController {
+
+	@Autowired
+    InitializationService initializationService;
 
 	@GetMapping("/")
 	public String index() {
 		return "HI HONGI";
 	}
+
+	@PostMapping("/initialize")
+    public void initializeParkingLot(){
+        initializationService.initialize();
+    }
 
 }
